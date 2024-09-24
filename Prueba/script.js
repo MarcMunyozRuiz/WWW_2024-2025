29e256cfed21e33be5f20c25be9b83ac6f803ab7
MarcMunyozRuiz <152491243+MarcMunyozRuiz@users.noreply.github.com>
2024-09-24 12:52:14 +0200

    Add files via upload

---
+++ Prueba/script.js
@@ -0,0 +1,12 @@
+function akali(){
+    let cadena1 = document.getElementById("cad1");
+    
+    if (cadena1.style.display === "none") {
+        cadena1.style.display = "block";
+      } 
+      
+        else {
+        cadena1.style.display = "none";
+      }
+     
+}
