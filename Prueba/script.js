797075ca43569fc57b21ab6f1ff11f292d3e8d40
MarcMunyozRuiz <152491243+MarcMunyozRuiz@users.noreply.github.com>
2024-11-22 11:58:15 +0100

    Update script.js

--- Prueba/script.js
+++ Prueba/script.js
@@ -1,12 +1,63 @@
 function akali(){
     let cadena1 = document.getElementById("cad1");
+    let cadena2 = document.getElementById("cad2");
     
     if (cadena1.style.display === "none") {
         cadena1.style.display = "block";
+        cadena2.style.display = "none"
       } 
       
         else {
         cadena1.style.display = "none";
+        cadena2.style.display = "block"
       }
      
+}
+
+function veigar(){
+  let cadena3 = document.getElementById("cad3");
+  let cadena4 = document.getElementById("cad4");
+  
+  if (cadena3.style.display === "none") {
+      cadena3.style.display = "block";
+      cadena4.style.display = "none"
+    } 
+    
+      else {
+      cadena3.style.display = "none";
+      cadena4.style.display = "block"
+    }
+   
+}
+
+function vex(){
+  let cadena5 = document.getElementById("cad5");
+  let cadena6 = document.getElementById("cad6");
+  
+  if (cadena5.style.display === "none") {
+      cadena5.style.display = "block";
+      cadena6.style.display = "none"
+    } 
+    
+      else {
+      cadena5.style.display = "none";
+      cadena6.style.display = "block"
+    }
+   
+}
+
+function aurora(){
+  let cadena7 = document.getElementById("cad7");
+  let cadena8 = document.getElementById("cad8");
+  
+  if (cadena7.style.display === "none") {
+      cadena7.style.display = "block";
+      cadena8.style.display = "none"
+    } 
+    
+      else {
+      cadena7.style.display = "none";
+      cadena8.style.display = "block"
+    }
+   
 }
