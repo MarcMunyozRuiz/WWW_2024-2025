aa283315e55a987056174922bc8416b9b11467e0
MarcMunyozRuiz <152491243+MarcMunyozRuiz@users.noreply.github.com>
2025-06-11 10:09:49 +0200

    Update script.js

--- Exercicis Javascript/7.1-For-Arrays/JS/script.js
+++ Exercicis Javascript/7.1-For-Arrays/JS/script.js
@@ -1,6 +1,15 @@
 function contar(){
     let cad = document.getElementById("cadena").value;
+    let contador = 0
 
+    for(let i = 0; i < cad.length; i++){
 
+        if(cad[i] == "a" ){
+            contador++
+        }
+    }
+
+    document.getElementById("resultado").innerHTML = `La frase tiene ${contador} letras a` ;
 }
+    
     
